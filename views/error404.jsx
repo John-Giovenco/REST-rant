7af5a440039b9b6fc3a13f9926bc806a0b5084c3
John-Giovenco <JohnGiovenco93@gmail.com>
2023-04-19 22:08:03 -0700

    issues with images and css

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,7 +6,13 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
+                <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                  <img src="/images/oops.jpg" alt="oops" />
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
           </main>
       </Def>
     )
