49107a3b73713f47f537859e78abe95f06152702
John-Giovenco <JohnGiovenco93@gmail.com>
2023-04-26 14:51:17 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -33,6 +33,19 @@ router.post('/', (req, res) => {
         pic: '/images/Restaurant_two.jpg'
   }]   
 */
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
@@ -56,14 +69,3 @@ router.delete('/:id/rant/:rantId', (req, res) => {
 })
 
 module.exports = router
-
-
-
-
-
-
-
-
-
-
-
