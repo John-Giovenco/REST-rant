391795378491efc2c99bf0e6ecd809e0756c1df1
John-Giovenco <JohnGiovenco93@gmail.com>
2023-04-26 13:36:14 -0700

    improving places.js

--- controllers/places.js
+++ controllers/places.js
@@ -3,6 +3,7 @@ const places = require('../models/places.js')
 
 router.post('/', (req, res) => {
   console.log(req.body)
+  res.send('POST /places stub')
   if (!req.body.pic) {
     // Default image if one is not provided
     req.body.pic = 'http://placekitten.com/400/400'
@@ -16,7 +17,8 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
-    let places = [{
+
+/*    let places = [{
         id:0,
         name: 'H-Thai-ML',
         city: 'Seattle',
@@ -30,7 +32,29 @@ router.post('/', (req, res) => {
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
         pic: '/images/Restaurant_two.jpg'
-  }]      
+  }]   
+*/
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+router.get('/:id', (req, res) => {
+    res.send('GET /places/:id stub')
+})
+router.put('/:id', (req, res) => {
+    res.send('PUT /places/:id stub')
+})
+router.delete('/:id', (req, res) => {
+    res.send('DELETE /places/:id stub')
+})
+router.get('/:id/edit', (req, res) => {
+    res.send('GET /places/:id/edit stub')
+})
+router.post('/:id/rant', (req, res) => {
+    res.send('GET /places/:id/rant stub')
+})
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
 
 module.exports = router
 
