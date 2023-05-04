6188a84d678a778e5f579b9463df42a25be564c7
John-Giovenco <JohnGiovenco93@gmail.com>
2023-05-03 17:43:08 -0700

    run seeder file

--- index.js
+++ index.js
@@ -25,8 +25,5 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}) 
-.then(() => { console.log('connected to mongo: ', process.env.MONGO_URI) }) 
-
 // Listen for Connections
 app.listen(process.env.PORT)
