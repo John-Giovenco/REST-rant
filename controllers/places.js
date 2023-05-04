0815e7a7f70f58915e2e5399c4c30a7dfcde6111
John-Giovenco <JohnGiovenco93@gmail.com>
2023-05-04 14:46:11 -0700

    year Validation

--- controllers/places.js
+++ controllers/places.js
@@ -42,13 +42,23 @@ router.get('/:id/edit', (req, res) => {
 
 router.post('/', (req, res) => {
   db.Place.create(req.body)
-    .then(() => {
+  .then(() => {
       res.redirect('/places')
-      })
-      .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
+  })
+  .catch(err => {
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+      }
+      console.log('Validation error message', message)
+      res.render('places/new', { message })
+  }
+  else {
+      res.render('error404')
+  }
+  })
 })
 
 router.get('/:id', (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,13 +5,29 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{ data.name }</h1>
-            <h2>Rating</h2>
-                <p>not rated</p>
-            <h3>
-                Located in {data.place.city}, {data.place.state}
-            </h3>
-                <p>No comments yet</p>
+            <div className="row">
+                <div className="col-sm-6">
+                    <img src={data.place.pic} alt={data.place.name} />
+                    <h3>
+                    Located in {data.place.city}, {data.place.state}
+                    </h3>
+                </div>
+            </div>
+            <div className="col-sm-6">
+                <h1>{ data.name }</h1>
+                <h2>Rating</h2>
+                    <p>not rated</p>
+                <h3>
+                    Located in {data.place.city}, {data.place.state}
+                </h3>
+                    <p>No comments yet</p>
+            </div>
+                <h3>
+                    {data.place.showEstablished()}
+                </h3>
+                <h4>
+                    Serving {data.place.cuisines}
+                </h4>
             <div>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                     Edit
@@ -28,3 +44,4 @@ function show (data) {
 }
 
 module.exports = show
+
